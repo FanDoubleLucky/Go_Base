96d0e56461b9c2cadcbd6085127652249168296c
FanDoubleLucky <1398328478@qq.com>
2019-06-08 11:39:13 +0800

    initial commit

---
+++ CloseChannel.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	jobs := make(chan int, 5)
+	done := make(chan bool)
+
+	go func() {
+		for {
+			j, more := <-jobs
+			if more {
+				fmt.Println("received job", j)
+			} else {
+				fmt.Println("received all jobs")
+				done <- true
+				return
+			}
+		}
+	}()
+
+	for j := 1; j <= 3; j++ {
+		jobs <- j
+		fmt.Println("sent job", j)
+	}
+	close(jobs) //关闭一个通道意味着不能再向这个通道发送值了，对从通道读值没影响
+	fmt.Println("sent all jobs")
+	<-done
+}
